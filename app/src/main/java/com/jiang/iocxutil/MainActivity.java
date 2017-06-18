b97bab0bd1726577c24a14f4d4f3024de518eb80
jiang <123>
2017-06-18 23:51:19 +0800

    IOC控制反转框架

--- app/src/main/java/com/jiang/iocxutil/MainActivity.java
+++ app/src/main/java/com/jiang/iocxutil/MainActivity.java
@@ -11,6 +11,10 @@ import com.jiang.iocxutil.annotion.ContentView;
 import com.jiang.iocxutil.annotion.OnClick;
 import com.jiang.iocxutil.annotion.ViewInject;
 
+/**
+ * IOC控制反转框架
+ *
+ */
 @ContentView(value = R.layout.activity_main)
 public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
